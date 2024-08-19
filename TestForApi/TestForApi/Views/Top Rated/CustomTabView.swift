36aae7c9d320debb01f94d422798c19d89c9bbb1
oviePeriscope <habibur.rahman@periscopelabs.io>
2024-08-19 22:14:06 +0600

    add customTabView

---
+++ TestForApi/TestForApi/Views/Top Rated/CustomTabView.swift
@@ -0,0 +1,18 @@
+//
+//  CustomTabView.swift
+//  TestForApi
+//
+//  Created by Habibur Rahman on 19/8/24.
+//
+
+import SwiftUI
+
+struct CustomTabView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CustomTabView()
+}

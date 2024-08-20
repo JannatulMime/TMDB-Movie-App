67eaba6ae11fe7d3002c81948c2f3271adea631b
oviePeriscope <habibur.rahman@periscopelabs.io>
2024-08-20 14:56:59 +0600

    add AiringToday

---
+++ TestForApi/TestForApi/Model/TvSeriesData.swift
@@ -0,0 +1,8 @@
+//
+//  TvSeriesApi.swift
+//  TestForApi
+//
+//  Created by Habibur Rahman on 20/8/24.
+//
+
+import Foundation
---
+++ TestForApi/TestForApi/Views/TVSeriesView/Airing Today/HorizontalSingleItem.swift
@@ -0,0 +1,18 @@
+//
+//  HorizontalSingleItem.swift
+//  TestForApi
+//
+//  Created by Habibur Rahman on 20/8/24.
+//
+
+import SwiftUI
+
+struct HorizontalSingleItem: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HorizontalSingleItem()
+}
